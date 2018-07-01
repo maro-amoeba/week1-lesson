13bdd12a8b67f7d099e84a0cd79948b488d26ca9
maro <amoeba3215da@gmail.com>
2018-07-01 16:43:59 +0900

    “python演習課題1”

---
+++ 03-01-python-sort.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+df = pd.read_csv('../Atom_Mydata/python-intro1/03-01-python-sort.csv')
+
+print(df)
+
+"""
+pythonのデータ分析ライブラリ『pandas』を用いている。
+格別記述することがないが、read_csv()のカッコ内のパスは絶対パスでももちろん良い。
+"""
